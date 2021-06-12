fcfa74ec4c8787280908463fd75b208dbfe2372f
David Anderson <dlande000@gmail.com>
2021-06-12 08:49:31 -0400

    Add template for displaying search results

--- client/Search/Search.jsx
+++ client/Search/Search.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SearchResult from './SearchResult';
 
-const Search = () => (
-  <>
-    <h2>Search</h2>
-  </>
-);
+const Search = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  return (
+    <>
+      <h2>Search</h2>
+      {searchResults.length ? 
+        searchResults.map((result, i) => (
+          <SearchResult
+            key={`searchResult${i}`}
+            user={result}
+          />
+        ))
+      : (
+        <div>
+          No users found who match your search. Bummer!
+        </div>
+      )}
+    </>
+  )
+};
 
 export default Search;
