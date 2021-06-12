06a3a8a87196b918a41fa5d655efbe2a6143b505
David Anderson <dlande000@gmail.com>
2021-06-12 09:42:15 -0400

    Add full search form

--- client/Search/Search.jsx
+++ client/Search/Search.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 
 import SearchResult from './SearchResult';
 
+const AGES = [];
+
+for (let i = 18; i < 100; i++) {
+  AGES.push(i);
+}
+
 const Search = () => {
   const [searchResults, setSearchResults] = useState([]);
 
@@ -15,7 +21,7 @@ const Search = () => {
       <div id="searchForm">
         <label>
           Search for musicians with whom to jam
-          <form>
+          <form onSubmit={handleSearch}>
             <label for="instruments">Choose an instrument:
               <select id="instruments" name="instruments">
                 <option value="vocals">Vocals</option>
@@ -29,7 +35,7 @@ const Search = () => {
                 <option value="string">String Instrument</option>
                 <option value="percussion">Percussion</option>
               </select>
-            </label>
+            </label><br/>
             <label for="genre">Choose a genres:
               <select id="genre" name="genre">
                 <option value="rock">Rock</option>
@@ -44,11 +50,39 @@ const Search = () => {
                 <option value="reggae">Reggaeton</option>
                 <option value="folk">Folk</option>
               </select>
-            </label>
-
-            <input type="text" name="email" placeholder="Email" />
-            <input type="password" name="password" placeholder="Password" />
-            <button type="button" onClick={handleSearch}>Search</button>
+            </label><br/>
+            <label for="skill">Skill level:
+              <select id="skill" name="skill">
+                <option value="amateur">Amateur</option>
+                <option value="professional">Professional</option>
+              </select>
+            </label><br/>
+            <label for="minAge">Be older than:
+              <select id="minAge" name="minAge">
+                {AGES.map((age, i) => (
+                  <option value={age}>{age}</option>
+                ))}
+              </select>
+            </label><br/>
+            <label for="maxAge">...but younger than (sorry, grandpa):
+              <select id="maxAge" name="maxAge">
+                {AGES.map((age, i) => (
+                  <option
+                    value={age}
+                    selected={age === 99}
+                  >
+                    {age}
+                  </option>
+                ))}
+              </select>
+            </label><br/>
+            <label>Location:
+              <input type="text" name="location" placeholder="NYC, LA, Outer space" />
+            </label><br/>
+            <label>Only show non-men
+              <input type="checkbox" id="gender" name="gender" />
+            </label><br/>
+            <input type="submit" value="Search" />
           </form>
         </label>
       </div>
